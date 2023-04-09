6e9fe08c82a63761aea7209e261f18fd9315cc10
Extro21 <vostrik21@gmail.com>
2023-04-09 15:34:30 +0300

    commit project

---
+++ app/src/main/java/com/practicum/homework1/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.practicum.homework1
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonSearch = findViewById<Button>(R.id.button_search)
+        val buttonMedia = findViewById<Button>(R.id.button_media)
+        val buttonSetting = findViewById<Button>(R.id.button_setting)
+
+
+        buttonSearch.setOnClickListener {
+            val displaySearch = Intent(this, SearchActivity::class.java)
+            startActivity(displaySearch)
+        }
+
+        buttonMedia.setOnClickListener {
+            val displayMedia = Intent(this, MediaActivity::class.java)
+            startActivity(displayMedia)
+        }
+
+        buttonSetting.setOnClickListener {
+            val displaySetting = Intent(this, SettingsActivity::class.java)
+            startActivity(displaySetting)
+        }
+
+    }
+}
